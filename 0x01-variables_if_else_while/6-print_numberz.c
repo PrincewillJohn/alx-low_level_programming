1ad96e2f85e971c1cf45dd5aba4c9480f6c4023f
PrincewillJohn <illuminecreativitynetwork@gmail.com>
2022-04-13 15:11:29 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: prints numbers zero to nine on one line
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 9; ++i)
+	putchar('0' + i);
+	purchar('\n');
+	return (0);
+}
