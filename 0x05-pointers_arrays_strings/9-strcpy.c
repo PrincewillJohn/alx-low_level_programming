11afb999729477a7d8f9d275ae5add3d6df99f4e
PrincewillJohn <illuminecreativitynetwork@gmail.com>
2022-04-24 11:44:04 -0700

    function that copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,12 +5,11 @@
  * @dest: char type string
  * @src: char type string
  *
- * Description: Copy the string pointed to by pointer `src` to
+ * Description:Copy the string pointed to by pointer `src` to
  * the buffer pointed to by `dest`
- * Return: Pointer to `dest`
  */
 
-char *_strpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 
