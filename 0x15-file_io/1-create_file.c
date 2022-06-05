4f343cf8a07f8657f558952c7d9310bbe6b527bb
PrincewillJohn <illuminecreativitynetwork@gmail.com>
2022-06-05 12:27:48 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* creates file or truncates a file, and give it permission -rw------ */
-	file = open(filename, O_RDWR | O_TRUNC | O_CRET, 0600);
+	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (file == -1)
 		return (-1);
 
